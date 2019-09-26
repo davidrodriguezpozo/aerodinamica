948189cf855600981801f876d456d869abe50bde
David Rodriguez <davidrp1996@gmail.com>
2019-09-26 11:30:27 +0200

    Update diffusion_u.m

--- diffusion_u.m
+++ diffusion_u.m
@@ -4,23 +4,21 @@ N = size(u,2);
 M = size(u,1);
 dx = L/N;
 dy = L/M;
-Se = Sw = dy;
-Ss = Sn = dx;
+Sw = dy;
+Sn = dx;
+Se = Sw;
+Ss = Sn; 
 
 for i = 1:N
-    for j=:1:M
+    for j=1:M
+        %Definim els valors de les velocitats als nodes W,E,S,N,P
+        uE = u(i+1,j);
+        uP = u(i,j);
+        uW = u(i-1,j);
+        uN = u(i,j+1);
+        uS = u(i,j-1);
 
-%Definim els valors de les velocitats als nodes W,E,S,N,P
-uE = u(i+1,j);
-uP = u(i,j);
-uW = u(i-1,j);
-uN = u(i,j+1);
-uS = u(i,j-1);
-
-du(i,j) = (uE - uP) / dx * Se + (uP-uW)/ dx * Sw + (uN-uP)/dy * Sn + (uP-uS)/dy * Ss;
-
-
-
+        du(i,j) = (uE - uP) / dx * Se + (uP-uW)/ dx * Sw + (uN-uP)/dy * Sn + (uP-uS)/dy * Ss;
     end
 end
 
