b6f3a0485d5f9655649f6d5c22a822e127a8e504
SergiMartinez5 <55831654+SergiMartinez5@users.noreply.github.com>
2019-10-09 22:32:38 +0200

    Compute_error.m with comments

--- Part A/Compute_error.m
+++ Part A/Compute_error.m
@@ -1,8 +1,17 @@
-function [error_du error_cu error_dv error_cv] = Compute_error ( S, k) 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%       FUNCTION COMPUTE_ERROR      %%%%%%%%%%%%%
+%%% This function computes, as it is said in the name,  %%%
+%%% the error between the numeric and analitic solution %%%
+%%% with the final purpose to validate the code of the  %%%
+%%% Part A.                                             %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+function [error_du error_cu error_dv error_cv] = Compute_error ( S, k) 
+% The Outputs: diffusive u term error, diffusive v term error, convective u term error, diffusive v term error.
+% The Inputs: the solution (S) 
 error_du = error(S.diffu,S.du_anal);
 error_cu = error(S.cu,S.cu_anal);
 error_dv = error(S.diffv,S.dv_anal);
 error_cv = error(S.cv,S.cv_anal);
-
-%disp('Error calculat');
