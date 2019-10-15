c22639e7996af998878fd07e638ad93579ddd551
SergiMartinez5 <55831654+SergiMartinez5@users.noreply.github.com>
2019-10-15 18:49:56 +0200

    Velocitats.m commented

--- Part A/Velocitats.m
+++ Part A/Velocitats.m
@@ -1,13 +1,28 @@
-function [u v ] = Velocitats (datos, C)
+  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%         FUNCTION VELOCITATS         %%%%%%%%%%%
+%%% This function, obviously, iterates between the stag-%%%
+%%% gered meshes computing the velocities in the face of%%%
+%%% this kind of meshes. We define the meshes using x or%%%
+%%% y values, imposing the condition in the main func-  %%%
+%%% tion.                                               %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+function [u v ] = Velocitats (datos, C)
+% The inputs: initial values (datos) and meshes definitons (c).
+% The outputs: u is the velocity for x-axis and v for y-axis.
+
+% Starting the double loop iteration for the x and y meshes-dimensions. 
 for i = 1:datos.Nx
     for j = 1:datos.Ny
-        
+        % For x or y staggered meshes. 
         x = C.stagX_x(i,j);
         y = C.stagX_y(i,j);
-        
+        % The velocity matrix in the x direction and staggered mesh is refilled.
         u(i,j) = datos.F*cos(2*pi*x)*sin(2*pi*y);
         
+        % Item for the part above. 
         x = C.stagY_x(i,j);
         y = C.stagY_y(i,j);
         
