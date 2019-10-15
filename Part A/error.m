cd328ad7259a72f8d217c9440c12801ddf70393a
SergiMartinez5 <55831654+SergiMartinez5@users.noreply.github.com>
2019-10-15 20:11:45 +0200

    error.m commented

--- Part A/error.m
+++ Part A/error.m
@@ -1,15 +1,26 @@
+  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%           FUNCTION ERROR          %%%%%%%%%%%%%
+%%% This function analyse the error between the numeric-%%%
+%%% al and the analytical solutions of the convective   %%%
+%%% and the diffusive terms of the NS equation. This job%%%
+%%% will be done substrating analitical and numerical   %%%
+%%% terms of each solution.                             %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 function error = ERROR(numeric,analytic)
-    
+ % The inputs: numeric and analytic terms of diffusive and convective solutions.
+ % The outputs: the error.
+ 
+ % Firstly, the error is initialized in case of avoid weird results.
     error = 0;
 
+ % In this double bucle, the developer intends to analyse the error in each position of the mesh. 
     for i = 1:size(numeric,1)
        for j = 1:size(numeric,2)
-           %if analytic(i,j) < 0.0001 ||  analytic(i,j) > -0.0001
-            %    a = abs(analytic(i,j)-numeric(i,j));
-             %   disp('Algo va mal');
-           %else 
                a = abs(analytic(i,j)-numeric(i,j));
-           %end
             if a > error 
                 error = a;
             end
