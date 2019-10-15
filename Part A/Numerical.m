eb166a34252950ca8ca71282a1fe25a1723e7c90
SergiMartinez5 <55831654+SergiMartinez5@users.noreply.github.com>
2019-10-15 18:40:05 +0200

    Numerical.m commented

--- Part A/Numerical.m
+++ Part A/Numerical.m
@@ -1,4 +1,16 @@
+  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%         NUMERICAL FUNCTION        %%%%%%%%%%%%%
+%%% This function computes the Numerical solution for   %%%
+%%% the convective and the diffusive term for the x-di- %%%
+%%% rection and the y-direction.                        %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 function [conv_u diff_u conv_v diff_v] = Numerical (datos, C, u, v)
+% The inputs: the initial values (datos), the meshes (C), and the velocity field computed in other function. 
+% The outputs: the convectives and diffusives terms. 
 
         conv_u = convection(u,v,datos.L,datos.H);
         diff_u = diffusion_u(u,datos.L);
