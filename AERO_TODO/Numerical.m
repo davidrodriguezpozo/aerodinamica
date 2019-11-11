2c462cb70af82db216d235e395755066fb5be6b7
SergiMartinez5 <55831654+SergiMartinez5@users.noreply.github.com>
2019-11-11 17:44:53 +0100

    Update Numerical.m

--- AERO_TODO/Numerical.m
+++ AERO_TODO/Numerical.m
@@ -1,3 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%           FUNCTION NUMERICAL       %%%%%%%%%%%%
+%%% This function just executes the functions which so- %%%
+%%% lute the convection and diffusive term.             %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [conv_u diff_u conv_v diff_v] = Numerical (datos, C, u, v)
 
         [conv_u conv_v] = convection_main(u,v,datos.L,datos.H,C);
