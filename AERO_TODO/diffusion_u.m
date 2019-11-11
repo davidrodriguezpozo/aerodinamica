be6beb4a1c3ed798babddad0bed0a9c4d2635ec0
SergiMartinez5 <55831654+SergiMartinez5@users.noreply.github.com>
2019-11-11 17:39:05 +0100

    Update diffusion_u.m

--- AERO_TODO/diffusion_u.m
+++ AERO_TODO/diffusion_u.m
@@ -1,5 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%           FUNCTION DIFUSSION       %%%%%%%%%%%%
+%%% This function computes the diffusive term of the    %%%
+%%% NS equation.                                        %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function du = diffusion_u(u,L)
-
+% Inputs: N (size of the x-axis "mesh"), M (size of the y-axis "mesh"),
+% dx and dy are the incrementals; and the Sx components are the Sufaces defined in the faces of the mesh. 
 N = size(u,1);
 M = size(u,2);
 du = zeros(N,M);
@@ -12,19 +19,15 @@ Ss = Sn;
 
 for i = 2:N-1
     for j=2:M-1
-        %Definim els valors de les velocitats als nodes W,E,S,N,P
+        % the velocity components are definied in each face. 
         uE = u(i,j+1);
         uP = u(i,j);
         uW = u(i,j-1);
         uN = u(i+1,j);
         uS = u(i-1,j);
-
+        % Finally, the diffusive term is computed for each point of the domain, using the values computed below. 
         du(i,j) = (uE - uP) / dx * Se - (uP-uW)/ dx * Sw + (uN-uP)/dy * Sn - (uP-uS)/dy * Ss;
       
-        %du(i,j)=(u(i,j+1)-u(i,j))/dx*dx-(u(i,j)-u(i,j-1))/dx*dx...
-        %  + (u(i+1,j)-u(i,j))/dx*dx-(u(i,j)-u(i-1,j))/dx*dx;
-      
-        %du(i,j)=(u(i,j+1)-u(i,j))/(dx)*(dx)-(u(i,j)-u(i,j-1))/(dx)*(dx)...
-        %  + (u(i+1,j)-u(i,j))/(dx)*(dx)-(u(i,j)-u(i-1,j))/(dx)*(dx);
+        
     end
 end
