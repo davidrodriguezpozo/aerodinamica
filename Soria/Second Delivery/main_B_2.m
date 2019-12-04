b2795471cecd466f4994ba4294eb073419d998c8
David Rodriguez <davidrp1996@gmail.com>
2019-12-04 16:05:08 +0100

    dfgdrfg

--- Soria/Second Delivery/main_B_2.m
+++ Soria/Second Delivery/main_B_2.m
@@ -1,20 +1,5 @@
 function main_B_2
 
-%Es la mateixa funcio que el main pero sense les divisions de VC pel M.M.S.
-
-%{  
-                                El Codi hauria de fer aix� 
-
- 1. Timestep for stability
- 2. u_n_1 i u_n conegudes --> R_n_1 i R_n
- 3. Evaluar u_p = u_n + ?t(3/2 * R_n - 1/2 * R_N_1
-        R = - Conv(u)/V + Diff(u)/V
- 4. Resoldre l'equaci� de Poisson A * pseudo_p = Div(u_p)
- 5. Obtenir pseudo_p
- 6. Obtenir u_n1 --> u_n1 = u_p - grad(pseudo_p) 
- 7. timestep + 1 
- %}
-
 set(groot, 'DefaultTextInterpreter','latex');
 set(groot, 'Defaultaxesticklabelinterpreter','latex');
 
@@ -48,6 +33,10 @@ delta_t = 0.3;
 
 [u_p, v_p, R_uant, R_vant] = predicted(u,v,delta_t,datos, conv_u, diff_u, conv_v, diff_v, R_uant, R_vant,R_u,R_v);
 
+u_p = zeros(Vx+2,Vx+2);
+u_p(3,3) = 1;
+v_p(3,3) = 1;
+
 div_u_p = divergencia_u(datos, u_p, v_p, nodal_mesh, num); %u_p = [Vx*Vy,1]
 
 pseudo_p = zeros(Vx*Vx,1); %Pseudo-pressure vector NOT KNOWN 
@@ -59,6 +48,7 @@ matriu_A = A_laplace(datos,C); %Pseudo-pressure matrix A. [Vx*Vy, Vx*Vy]
 matriu_A(1,1)=-5;
 
 pseudo_p = inv(matriu_A)*div_u_p; %pseudo_p = [Vx*Vy,1]
+
 
 [p_gradX, p_gradY] = gradient_p(datos, pseudo_p, nodal_mesh); 
 p_gradX = haloupdate(p_gradX);
